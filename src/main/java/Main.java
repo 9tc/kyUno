490c2fd3ee0effe8d4083c82a905de19fdc59cda
9tc <>
2019-10-21 21:31:00 +0900

    update matchEnding

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args){
         Game g = new Game(0,7);
         Scanner sc = new Scanner(System.in);
-        while (true){
+        do {
             g.run();
             System.out.print("もう一度プレイする? (Yes:1, No:0) : ");
-            if (!sc.nextLine().matches("1"))break;
-        }
+        } while (sc.nextLine().matches("1"));
         System.out.println("ゲームを終了しました");
     }
 }
